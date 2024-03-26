5921cef3683c98f9f03559a5158eb7da579031c8
Rubie66 <1476747911@qq.com>
2024-03-26 16:23:32 +0800

    3.27

--- 01-arrays/src/main/java/oy/tol/tra/Grades.java
+++ 01-arrays/src/main/java/oy/tol/tra/Grades.java
@@ -31,13 +31,7 @@ public class Grades {
        5. Fix the issue.
        6. Transform the algorithm to <strong>use</strong> the generic one from Algorithms.java, as instructed in the readme file.
       */
-      int i = 0;
-      while (i < grades.length/2) {
-         int temp = grades[i];
-         grades[i] = grades[grades.length-i-1];
-         grades[grades.length-i-1] = temp;
-         i++;
-     }
+      Algorithms.reverse(grades);
    }
 
    /**
@@ -52,19 +46,7 @@ public class Grades {
        5. Fix the issue.
        6. Transform the algorithm to <strong>use</strong> the generic one from Algorithms.java as instructed in the readme file.
       */
-      int i = 0;
-      while (i < grades.length) {
-         int j = grades.length - 1;
-         while (j > i) {
-         if (grades[j] < grades[j-1]) {
-            int tmp = grades[j];
-            grades[j] = grades[j-1];
-            grades[j-1] = tmp;
-         }
-         j--;
-      }
-      i = i + 1;
-      }
+      Algorithms.sort(grades);
    }
 
    /**
